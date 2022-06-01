d0c97f42e46a8cef9513f5181442b92bf0058435
ITdevJyrgal <>
2022-06-01 12:22:47 +0600

    main

--- src/components/main/home/Teachers.js
+++ src/components/main/home/Teachers.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Slider from "react-slick";
 import {data} from "./dataTeachers";
 import axios from "axios"
+import {api} from "../../../http/api"
 
 const Teachers = () => {
     const settings = {
@@ -46,7 +47,7 @@ const Teachers = () => {
     };
     const [datas, setDatas] = useState([])
     useEffect(() => {
-        axios.get("https://school-prod.herokuapp.com/teacher/")
+        api.get("/teacher/")
             .then(({data}) => {
                 setDatas(data)
             })
